7d437f0d4c7f90e32aeec529052856194478a7ac
s134272 <s134272@student.dtu.dk>
2018-11-15 01:20:39 +0100

    Tried to implement smarter configfile and outputfile auto-naming

--- src/simulationRunner.java
+++ src/simulationRunner.java
@@ -15,24 +15,24 @@ public class simulationRunner {
 	private static double tStart = 1;
 	private static double hStart = 0;
 
-	private static double dT = 5;
-	private static double dH = 2;
+	private static double dT = 4;
+	private static double dH = 4;
 
 	private static double tEnd = 50;
-	private static double hEnd = hStart + 1;
+	private static double hEnd = 50;
 
 	private static int tArrSize = (int) Math.ceil((tEnd - tStart) / dT);// Numbers of temperatures to simulate
 	private static int hArrSize = (int) Math.ceil((hEnd - hStart) / dH);// Numbers of fields to simulate
 
 	private static double[][] T_H_Arr;
 
-	private static String location = "C:\\Users\\anders\\Documents\\11_Semester\\Speciale\\Data\\Ni_Check_2\\";
-	private static String name = "Ni_sim_2,2,2";
+	private static String location = "C:\\Users\\anders\\Documents\\11_Semester\\Speciale\\Data\\temp\\";
+	private static String name = "Ni_sim";
 	private static String configFilename = "config";
 	private static File logFile = new File("C:\\Users\\anders\\Documents\\11_Semester\\Speciale\\Data\\logFile.txt");
 	
 	private static boolean saveConfig = false;
-	private static boolean loadConfig = false;
+	private static boolean loadConfig = true;
 	private static int N = tArrSize * hArrSize; // Total numbers of simulations;
 
 	static Visualizer vis;
@@ -59,8 +59,14 @@ public class simulationRunner {
 		// [ -, -, -, -, -, -] Pure Co []
 
 		for (int i = 0; i < N; i++) {
-			String filename = String.format(name + "_T=%1.2f_and_H=%1.2f", T_H_Arr[i][0], T_H_Arr[i][1]);
-			paramRange[i] = new Parameters(new int[] { 2 << 17, 2, 2, 2 },
+			String filename = name + String.format("_T=%1.2f_H=%1.2f", T_H_Arr[i][0],T_H_Arr[i][1]); 
+					/*String.format("_(%d,%d,%d)_T=%1.2f_H=%1.2f.txt", 
+					paramRange[i].nX/2, 
+					paramRange[i].nY/2, 
+					paramRange[i].nZ/2, 
+					paramRange[i].temp,
+					paramRange[i].H.z);*/ // TODO required re-writing the simulator constructor.
+			paramRange[i] = new Parameters(new int[] { 2 << 20, 2, 2, 2 },
 					new double[] { T_H_Arr[i][0], 0, 0, T_H_Arr[i][1] },
 					new boolean[] { true },
 					new String[] {
@@ -73,25 +79,30 @@ public class simulationRunner {
 
 		sim = new Simulator[N];
 		threads = new Thread[N];
-
 		long startTime = System.currentTimeMillis();
 		if (parallel) {
 			for (int i = 0; i < N; i++) {
 				if(loadConfig) {
-					String configFile = location + name + String.format("_config_T=%1.2f_H=%1.2f.txt", T_H_Arr[i][0], T_H_Arr[i][1]);
+					String configFile = location + name + 
+							String.format("_%d,%d,%d_config_T=%1.2f_H=%1.2f.txt", 
+									paramRange[i].nX/2, 
+									paramRange[i].nY/2, 
+									paramRange[i].nZ/2, 
+									paramRange[i].temp,
+									paramRange[i].H.z);
 					sim[i] = new Simulator(paramRange[i], configFile);
 				} else {
 					sim[i] = new Simulator(paramRange[i]);	
 				}
-				sim[i].configFromBasisState(Crystal.Fz); // Forces a basis state onto the state
+				sim[i].configFromBasisState(Crystal.Cz); // Forces a basis state onto the state
 				threads[i] = new Thread(sim[i]);
 				threads[i].start();
 			}
 			// Visualizing:
-			int vIndex = 0;
-			vis = new Visualizer(sim[vIndex]);
-			String[] myArgs = { "test.Visualizer" };
-			PApplet.runSketch(myArgs, vis);
+//			int vIndex = 0;
+//			vis = new Visualizer(sim[vIndex]);
+//			String[] myArgs = { "test.Visualizer" };
+//			PApplet.runSketch(myArgs, vis);
 			
 			// Waiting message:
 			waitMessage(startTime, 0);
@@ -100,7 +111,12 @@ public class simulationRunner {
 				writeToLogFile(sim[i]);
 				if (saveConfig) {
 					String configFile = location + name
-							+ String.format("_config_T=%1.2f_H=%1.2f.txt", T_H_Arr[i][0], T_H_Arr[i][1]);
+							+ String.format("_(%d,%d,%d)_config_T=%1.2f_H=%1.2f.txt", 
+									paramRange[i].nX/2, 
+									paramRange[i].nY/2, 
+									paramRange[i].nZ/2, 
+									paramRange[i].temp,
+									paramRange[i].H.z);
 					sim[i].saveConfig(configFile);
 				}
 			}
