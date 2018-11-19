568a2d3093aca656c32c3447e2611fcd4cbd8c35
s134272 <s134272@student.dtu.dk>
2018-11-19 16:03:27 +0100

    Changed aggregate declaration to constructor, removed printFullArray
    Organized imports, made aggregate setable in constructor, removed
    superfluous printFullArray variable, and made the aggregate variable a
    solid part of the parameters.
    Have begun work on a function to load in parameters from a string in a
    file, such that a single file with a lot of parameters for simulations
    can be run.

--- src/main/Parameters.java
+++ src/main/Parameters.java
@@ -1,16 +1,19 @@
 package main;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 import constants.Crystal;
-import processing.core.PVector;
 
 public class Parameters {
 
 	/** Number of steps aggregation and cell dimensions */
 	
 	public int nSteps;
-	public int aggregate = 2 << 10;
+	public int aggregate;
 	public int nX = 1; // TODO Shoud be changed to be half as large and all references to be 2*nX
 	public int nY = 1;
 	public int nZ = 1;
@@ -29,24 +32,23 @@ public class Parameters {
 	public Config initial;
 
 	// Boolean options:
-	public boolean printFullArray;
 	private boolean perBoundsX = true;
 	private boolean perBoundsY = true;
 	private boolean perBoundsZ = true;
 
-	public static final int nIntParam = 4;
+	public static final int nIntParam = 5;
 	public static final int nFloatParam = 4;
-	public static final int nBoolParam = 1;
+	public static final int nBoolParam = 0;
 
 	public Crystal basis;
 
-	private static final String[] paramNames = { "#Steps", "nX", "nY", "nZ", "Temperature", "Hx", "Hy", "Hz", "Save config" };
+	private static final String[] paramNames = { "#Steps", "Aggregate", "nX", "nY", "nZ", "Temperature", "Hx", "Hy", "Hz" };
 //	public final double[] Jlist;
 
 	/**
 	 * Initialize with parameters: arrI: #steps, nX, nY, nZ arrF: temperature, Hx, Hy, Hz \n
 	 */
-	public Parameters(final int[] arrI, final double[] arrF, final boolean[] options, final String[] strings) {
+	public Parameters(final int[] arrI, final double[] arrF, final String[] strings) {
 		if (arrI.length != nIntParam || arrF.length != nFloatParam) {
 			String[] paramNames = getNames();
 			String st = new String("");
@@ -61,11 +63,10 @@ public class Parameters {
 		H = new MyVector(arrF[1], arrF[2], arrF[3]);
 
 		nSteps = arrI[0];
-		nX = 2*arrI[1];
-		nY = 2*arrI[2];
-		nZ = 2*arrI[3];
-
-		printFullArray = options[0];
+		aggregate = arrI[1];
+		nX = 2*arrI[2];
+		nY = 2*arrI[3];
+		nZ = 2*arrI[4];
 		
 		dir = strings[0];
 		filename = strings[1]; // TODO Re write such that outputfile is directly given to simmulator.
@@ -77,7 +78,7 @@ public class Parameters {
 	}
 
 	public double[] asList() {
-		return new double[] { nSteps, nX, nY, nZ, temp, H.x, H.y, H.z };
+		return new double[] { nSteps, aggregate, nX, nY, nZ, temp, H.x, H.y, H.z };
 	}
 
 	private String getParam(int i) {
@@ -85,21 +86,21 @@ public class Parameters {
 		case (0):
 			return Integer.toString(nSteps);
 		case (1):
-			return Integer.toString(nX);
+			return Integer.toString(aggregate);
 		case (2):
-			return Integer.toString(nY);
+			return Integer.toString(nX);
 		case (3):
-			return Integer.toString(nZ);
+			return Integer.toString(nY);
 		case (4):
-			return Double.toString(temp);
+			return Integer.toString(nZ);
 		case (5):
-			return Double.toString(H.x);
+			return Double.toString(temp);
 		case (6):
-			return Double.toString(H.y);
+			return Double.toString(H.x);
 		case (7):
-			return Double.toString(H.z);
+			return Double.toString(H.y);
 		case (8):
-			return Boolean.toString(printFullArray);
+			return Double.toString(H.z);
 		default:
 			return "";
 		}
@@ -115,5 +116,24 @@ public class Parameters {
 
 	public void setInitConfig(Config newConfig) {
 		this.initial = newConfig;
+	}
+	
+
+	public static Parameters loadParameters(String dir, String filename) throws IOException {
+		File file = Paths.get(dir, filename + ".txt").toFile();
+		BufferedReader in = new BufferedReader(new FileReader(file));
+		
+		int[] intParam = new int[Parameters.nIntParam];
+		double[] floatParam = new double[Parameters.nFloatParam];
+		for (int i = 0; i < intParam.length; i++) {
+			String s = in.readLine();
+			intParam[i] = Integer.parseInt(s);
+		}
+		for (int i = 0; i < intParam.length; i++) {
+			String s = in.readLine();
+			floatParam[i] = Double.parseDouble(s);			
+		}
+		in.close();
+		return new Parameters(intParam, floatParam, null);
 	}
 }
