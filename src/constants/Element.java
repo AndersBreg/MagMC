63ebeb97ac814e1cfb69fe06c7198dea5a279114
s134272 <s134272@student.dtu.dk>
2018-11-19 16:06:00 +0100

    Created toString method, which prints the element parameters

--- src/constants/Element.java
+++ src/constants/Element.java
@@ -2,14 +2,19 @@ package constants;
 
 public enum Element {
 	//Ja, Jb, Jc, Jbc, Jac, Jab, Da, Db, Dc, spin
-	Ni(0, 0.67, -0.05, 1.04, -0.11, 0.3, 0.339, 1.82, 0, 1.0),
-	Co(0, 0.05, 0, 0.35, -0.0728, -0.1131, 2.0277, 0, 0.8576, 1.5),
+	Ni(0, 0.67, -0.05, 1.04, -0.11, 0.30, 0.339, 1.82, 0, 1.0),
+	Co(0, 0.105, 0.194, 0.743, -0.163, -0.181, 0.718, 0, 0.802, 1.5),
 	Fe(0, 0.3, 0.14, 0.77, 0.05, 0.14, 1.62, 0, 0.56, 2),
-	Test(0, 0, 0, 0, 0, 0, 0, 0, 1.0, 1.0); // Without DM interactions
+	Test(0, 0, 0, -1, 0, 0, 0.0, 0.0, -1.0, 1.0); // Without DM interactions
 	//      Ja, Jb, Jc, Jbc, Jac, Jab [Da, Db, Dc]: 
 	// Co: [0, -0.23, 0, -0.46, 0, 0], [0, 0, 0] from Ellen 2017
-	// Co: [0, 0.105, 0.194, 0.743, -0.163, -0.181], [0.718,0.802,0] from Wei Tian 2008
-	// Co: [0.0446, 0.0549, 0, 0.3, -0.09, -0.01183], [2.17, 0, 1.25] from Wenjie Wan 2018
+	// Co: [0, 0.105, 0.194, 0.743, -0.163, -0.181], [0.718, 0, 0.802] from Wei Tian 2008
+	
+	// Co: [0.0453, 0.0549, 0, 0.3, -0.09, -0.1183], [2.17, 0, 1.25] from Wenjie Wan 2018 With DM interaction
+	
+	// Co: [0.0446, 0.0539, 0, 0.3, -0.0900, -0.1183], [2.1726, 0, 1.2518] from Wenjie Wan 2018 Without DM interaction
+	// Co: [0,-0.06935, -0.1858, -0.45937, 0, 0], [None given] from Kharchenko 2010
+	// Co neel temperature = 21.6 K, which gives that all constants must be < 0.827 meV
 	// (0.5828, 0.3646, 0.2541)
 	
 	public double spin;
@@ -45,6 +50,15 @@ public enum Element {
 		return (this.Jlist[n] + at.Jlist[n])/2; //this.paramList + at.paramList;
 	}
 
+	public String toString() {
+		String s = "[";
+		for (int i = 0; i < Jlist.length; i++) {
+			s = s + Jlist[i]+", ";
+		}
+		s += "], ["+Dx+", "+Dy+", "+Dz+"]";
+		return s;
+	}
+	
 	int getId() {
 		switch (this) {
 		case Ni:
